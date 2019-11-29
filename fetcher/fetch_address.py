8da08f6b820731ee698fc836a04a95861de5eb28
silverkip <xilvkip@gmail.com>
2019-11-29 11:03:45 +0900

    Added Address Fetcher

--- fetcher/fetch_address.py
+++ fetcher/fetch_address.py
@@ -1,8 +1,63 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+import googlemaps
 
-baseURL = r"https://www.navitime.co.jp/category/0201/13/"
+gmaps = googlemaps.Client(key='AIzaSyCjIjyA2rUsd7W_VjGcijvEzrq-wx4K4p4')
 
-htmlSource = requests.get(baseURL)
-soup = BeautifulSoup(htmlSource.content, 'html.parser')
-print(soup.prettify())
+baseURL = r"https://www.navitime.co.jp/category/0201/131"
+
+tokyo23 = [
+  "chiyoda",
+  "chuuo",
+  "minato",
+  "shinjuku",
+  "bunkyo",
+  "taito",
+  "sumida",
+  "koto",
+  "shinagawa",
+  "meguro",
+  "ota",
+  "setagaya",
+  "shibuya",
+  "nakano",
+  "suginami",
+  "toshima",
+  "kita",
+  "arakawa",
+  "itabashi",
+  "nerima",
+  "adachi",
+  "katsushika",
+  "edogawa",
+]
+
+for i in range(0,len(tokyo23)):
+  wardCode = i+1
+  wardURL = baseURL + str(f'{wardCode:02}') + "/"
+  with open('data/{0}-{1}.csv'.format(f'{wardCode:02}', tokyo23[i]), 'w') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(["Name", "Address", "Location"])
+    page = 1
+    while True:
+      print("Fetching page " + str(page) + " of " + tokyo23[i] + " ward ......")
+      htmlSource = requests.get(wardURL + "?page=" + str(page))
+      soup = BeautifulSoup(htmlSource.content, 'html.parser')
+      addresses = soup.find_all('dd', class_="address_name")
+      names = soup.find_all('dt', class_="spot_name")
+      if len(addresses) == 0:
+        break
+      for j in range(0, len(names)):
+        name = names[j].text
+        address = addresses[j].text[:-5]
+        try:
+          geo = gmaps.geocode(address)[0]['geometry']['location']
+        except:
+          print(name, address)
+          geo = "Error"
+        if ('PR' in name) or ('株' in name):
+          print(name, address, geo)
+          continue
+        spamwriter.writerow([name, address, geo])
+      page += 1
